61a7507b76af1455c283a9bb8469bfe4ec61936f
vvzt <vvzt666666@foxmail.com>
2019-05-06 00:11:51 +0800

    test point of trace map

--- src/renderer/components/visual-charts/map/map-3.vue
+++ src/renderer/components/visual-charts/map/map-3.vue
@@ -22,12 +22,12 @@ export default {
   created() {
   },
   async mounted() {
-    let arr = [[114.393114, 30.485042], [114.391114, 30.485542]]
+    let arr = [[114.39152098432997, 30.48170701012443], [114.3906380034553, 30.479940534750778], [114.39483425772288, 30.486846674282344]];
     await this.setMap();
     this.setAllMarketPoints();
-    // this.setMapPoints(arr);
-    // this.setMapLines(arr);
-    // this.setPointInfo();
+    this.setMapPoints(arr);
+    this.setMapLines(arr);
+    this.setPointInfo();
   },
   data() {
     return {
@@ -83,7 +83,7 @@ export default {
         div.className = 'point-info'
         div.innerHTML = `
           <div class='point-info--close' onclick='closePointInfo()'>x</div>
-          <p>地点：${content.position.join(',')}</p>
+          <p>地点：${content.position}</p>
           <p>消费次数：${content.count}</p>
           <p>最后消费时间：${content.last}</p>
           <p>总消费金额：${content.total}</p>
@@ -104,27 +104,31 @@ export default {
       });
     },
     setAllMarketPoints() {
-      let data = markets.map(item => item.slice(3, 6));
+      let data = markets.filter(item => item[3] && item[4] && item[5]); // 3 - 店名， 4 - 经度， 5 - 纬度
       let markersData = {};
-      data.filter(item => item[0] && item[1] && item[2]).map(item => {
-        if(markersData[item[0]]) {
-          // markers[item[0]]['coord'].push(item.slice(1));
+      data.map(item => {
+        let marketName = item[3], marketLng = item[4], marketLat = item[5];
+        let key = marketLng + '/' + marketLat;
+
+        // 将重叠的店合并 只显示一个图标 店名放一起显示
+        if(markersData[key]) {
+          markersData[key]['names'].add(marketName);
         } else {
-          markersData[item[0]] = {
-            'name': item[0],
+          markersData[key] = {
+            'names': new Set([marketName]),
             // 'coord': [item.slice(1)]
-            'coord': transPosFn(...item.slice(1))
+            'coord': transPosFn(marketLng, marketLat), // 转为高德地图经纬度
           };
         }
       });
       // let markerArr = [];
-      let markers = Object.keys(markersData).map(key => {
-        let item = markersData[key];
+      console.log(markersData)
+      let markers = Object.values(markersData).map(item => {
         let coord = [item.coord.lng, item.coord.lat];
-        let name = item.name;
+        let names = Array.from(item.names);
         let marker = new AMap.Marker({
           position: coord,
-          title: name,
+          title: names,
           icon: this.marketIcon,
           // offset: markerArr.indexOf(coord.toString()) > -1 ? new AMap.Pixel(100*Math.random(), 100*Math.random()) : new AMap.Pixel(0, 0),
         });
